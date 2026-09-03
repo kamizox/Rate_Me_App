b19cb30e20c4e43290a9672b8c31a06071e1d87e
kamizox <kamrankhan0368@gmail.com>
2026-09-03 22:37:23 +0500

    Login nav check

--- App.jsx
+++ App.jsx
@@ -5,11 +5,12 @@ import React from 'react'
 import {CustomButton} from './src/components/CustomButton.jsx'
 import {COLORS} from './src/constant/colors.js'
 import { styles } from './src/styleSheet.js'
+import {LoginScreen} from '../Pr/src/screens/auth/LoginScreen.jsx'
 
 
 const Stack = createNativeStackNavigator()
 
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor: "white" }}>
       <Image source={require ('./src/assets/OnboardingImage.png')}
@@ -20,7 +21,8 @@ function HomeScreen() {
       </Text>
       <View style= {{width: '100%'}}>
       <CustomButton title="Get Started" bgColor= {COLORS.primary} textColor="white" marginTop = {33} />
-      <CustomButton title="Log in" bgColor= {COLORS.white} textColor="black" marginTop = {23} borderwith= {1}/>
+      <CustomButton title="Log in" bgColor= {COLORS.white} textColor="black" 
+       marginTop = {23} borderwith= {1} onPress={()=> navigation.navigation('login')}/>
       </View>
       <Text style= {{marginTop: 10}} >By continuing you agree to our </Text>
       <Text style={{fontWeight:'bold'}}>Terms & Privacy Policy</Text>
@@ -33,6 +35,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name='login' component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
